ae465329881ab6fe171325c8179225552a54c5b7
xxValiumxx <takeyourvalium@gmail.com>
2013-08-09 21:26:36 -0700

    oops

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'b650f55b407ca81ff86a040c5e64638ef20aabcb725b2700087055a266c8694a01c72c95ce79c8fb9139e278c571d7e3ffb54210e1e4da65838103c57672c850'
